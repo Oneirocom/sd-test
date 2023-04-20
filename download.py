d7436201480bea49d8cde2642f75a62795f81178
coffeeorgreentea <90069434+coffeeorgreentea@users.noreply.github.com>
2023-04-20 01:26:27 -0400

    Update download.py

--- download.py
+++ download.py
@@ -6,7 +6,7 @@ def download_model():
     HF_AUTH_TOKEN = os.getenv("HF_AUTH_TOKEN")
     repo = 'spaceinvader/fb'
     scheduler = DPMSolverMultistepScheduler.from_pretrained(repo, subfolder="scheduler")
-    model = DiffusionPipeline.from_pretrained(repo, torch_dtype=torch.float16, revision="fp16", scheduler=scheduler, use_auth_token=HF_AUTH_TOKEN) 
+    model = DiffusionPipeline.from_pretrained(repo, torch_dtype=torch.float16, revision="main", scheduler=scheduler, use_auth_token=HF_AUTH_TOKEN) 
     
 if __name__ == "__main__":
     download_model()
