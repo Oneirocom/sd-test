64fba475ebb8fe09f6fbe7e976376f1b05eb0e97
coffeeorgreentea <90069434+coffeeorgreentea@users.noreply.github.com>
2023-04-20 01:18:04 -0400

    Update app.py

--- app.py
+++ app.py
@@ -8,7 +8,7 @@ import os
 def init():
     global model
     HF_AUTH_TOKEN = os.getenv("HF_AUTH_TOKEN")
-    repo = 'stabilityai/stable-diffusion-2-1-base'
+    repo = 'spaceinvader/fb'
     scheduler = DPMSolverMultistepScheduler.from_pretrained(repo, subfolder="scheduler")
     model = DiffusionPipeline.from_pretrained(repo, torch_dtype=torch.float16, revision="fp16", scheduler=scheduler, use_auth_token=HF_AUTH_TOKEN).to("cuda")    
 
